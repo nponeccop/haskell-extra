55cf90845f9c70a5e508816a1ef45f4195ab0af4
Andy Melnikov <andy.melnikov@gmail.com>
2014-12-23 21:00:09 +0200

    more packages

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo building $1
 LO=$(echo $1 | tr '[:upper:]' '[:lower:]')
 cblrepo list $1
-cblrepo pkgbuild $1
-cd haskell-$LO && makepkg -c && mkaurball
+cblrepo pkgbuild --patchdir patches $1
+cd haskell-$LO && makepkg -s -c && mkaurball
 yaourt -U $(ls -1v *.pkg.tar.xz | tail -n 1)
