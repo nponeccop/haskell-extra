c5fe147d71ec9914aa9d3ec10312421f84450261
Andy Melnikov <andy.melnikov@gmail.com>
2014-12-23 12:12:25 +0200

    initial packerlib

--- auto.sh
+++ auto.sh
@@ -1,1 +1,9 @@
+PACKERLIB=1 
+. ./packerlib
+LO=haskell-$(echo $1 | tr '[:upper:]' '[:lower:]')
+
+[[ -d $LO ]] && echo $LO is already built && exit
+
+(existsinpacman $LO || providedinpacman $LO) && echo "$LO exists in repo" && exit
+
 (bash version.sh $1 | xargs bash add.sh) && bash build.sh $1
