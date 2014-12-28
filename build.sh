42c34916f2ba5669145d42112d25e924132a14e0
Andy Melnikov <andy.melnikov@gmail.com>
2014-12-29 01:46:26 +0200

    yesod-auth, yesod-form

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ set -ex
 PKG=$(echo $1 | tr '[:upper:]' '[:lower:]')
 cblrepo pkgbuild $1 --ghc-version 7.8.4
 cd haskell-$PKG
-makepkg -sci --needed --asdeps -L 
+makepkg -sc --needed --asdeps -L 
+yaourt -U $(ls -1v *.pkg.tar.xz | tail -n 1)
 mkaurball
 mv *.src.tar.gz ../aur/
 
