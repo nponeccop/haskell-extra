d8e095ea10b416b4351ddbc4fd4959867d7f5cee
Andy Melnikov <andy.melnikov@gmail.com>
2014-12-29 02:00:30 +0200

    hspec-discover, yesod-routes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -ex
 PKG=$(echo $1 | tr '[:upper:]' '[:lower:]')
 cblrepo pkgbuild $1 --ghc-version 7.8.4
 cd haskell-$PKG
-makepkg -sc --needed --asdeps -L 
+makepkg -sc --needed --asdeps
 yaourt -U $(ls -1v *.pkg.tar.xz | tail -n 1)
 mkaurball
 mv *.src.tar.gz ../aur/
