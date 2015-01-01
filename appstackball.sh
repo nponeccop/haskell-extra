2c4039fd4c15655ea8f2f1f8848aa8a6fc1fb9a3
Andy Melnikov <andy.melnikov@gmail.com>
2015-01-01 19:43:14 +0200

    appar was adopted by haskell-core

--- appstackball.sh
+++ appstackball.sh
@@ -1,7 +1,6 @@
-cd appstack
+pushd upstream-repos/haskell-happstack
 cblrepo pkgbuild --ghc-version=7.8.4 $1
 cd haskell-$(echo $1 | tr '[:upper:]' '[:lower]')
 mkaurball
-mv *.src.tar.gz ../../aur/
-cd ../..
-
+mv *.src.tar.gz ../../../aur/
+popd
--- coreball.sh
+++ coreball.sh
@@ -1,7 +1,6 @@
-cd core
+pushd upstream-repos/haskell-core
 cblrepo pkgbuild --ghc-version=7.8.4 $1
 cd haskell-$(echo $1 | tr '[:upper:]' '[:lower]')
-mkaurball
-mv *.src.tar.gz ../../aur/
-cd ../..
-
+mkaurball -f
+mv *.src.tar.gz ../../../aur/
+popd
